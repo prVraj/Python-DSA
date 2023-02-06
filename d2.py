a4d9b9432217231a9ba0d6e0f85405d568510579
Vraj Parikh <106619746+prVraj@users.noreply.github.com>
2023-02-06 23:48:20 +0530

    Add files via upload

---
+++ d2.py
@@ -0,0 +1,16 @@
+# this for string where we want to add somme cotetion in sentence
+h = "hello \"kem\" cho!!"   
+print(h)
+
+# this for write a poem
+h = """hello 
+kem 
+cho!!"""   
+print(h)
+
+# this for additional fun.
+
+u = "vraj vvv vv vraj vraj varjvccydv."
+print(u.count("vr"))    #to count any value or string repeatation in variable.
+print(u.center(200))    #to shift your string in center as your requirement.
+print(u.find("ar"))        #to find any value or string in variable.
