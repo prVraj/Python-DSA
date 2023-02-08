9c8c90cfa472e3db43f9658fc67596804e3c512e
Vraj Parikh <106619746+prVraj@users.noreply.github.com>
2023-02-09 00:54:42 +0530

    check statement
    check statement is newly introduced in python version 3.10 
    this is same as switch statement in  C, C++ , JAVA.

---
+++ p4.py
@@ -0,0 +1,17 @@
+# we can create such multiple choises Questions (MCQ)
+
+print('What is 2+3?')
+no = int(input("Enter Your ANS from given below:\n1. 4\t2. 6\t3.5\t4.23\n"))
+match no:
+    case 1:
+        print('your ANS is wrong!!!!')
+    case 2:
+        print('your ANS is wrong!!!!')
+    case 3:
+        print('you are right!!!! (*-*)')
+    case 4:
+        print('your ANS is wrong!!!!')
+    
+# Also we can create so many differrent things like we can add if-else statements.
+
+    # case  case_no  if  variable_name  condition: 
