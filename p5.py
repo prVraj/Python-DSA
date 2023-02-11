89043a2aeedb0871185832c933ad3f15d56fc98b
Vraj Parikh <106619746+prVraj@users.noreply.github.com>
2023-02-11 23:21:51 +0530

    for_loop_with_6table

---
+++ p5.py
@@ -0,0 +1,26 @@
+""" Now we talk about loop 
+    Their are two types of loop:
+        1. for loop
+        2. while loop"""
+
+
+
+# for loop
+
+
+# in range fun - print no of given range
+
+for i in range(10):
+    print(i + 1 , end=" ")
+print()                                            # this for new line
+
+# now for string
+
+name = 'prVraj'
+for v in name:                                      # Here we can give any varible replace to 'v' 
+    print(v)
+
+# now for table of 6
+
+for k in range(10):
+    print( '6 *',k+1,'=',6*(k+1) )
